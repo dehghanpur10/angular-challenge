17833ec506616c04f06a290ec33265f888469a8b
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-08-26 21:40:35 +0430

    add test for loading componet and loading service

--- src/app/shared/loading/loading.component.spec.ts
+++ src/app/shared/loading/loading.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 import { LoadingComponent } from './loading.component';
 
@@ -11,15 +13,25 @@ describe('LoadingComponent', () => {
       declarations: [ LoadingComponent ]
     })
     .compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(LoadingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  it('should be display error', ()=>{
+    component.isLoading = true;
+    fixture.detectChanges();
+    const errorDebug: DebugElement = fixture.debugElement;
+    const div:HTMLElement  = errorDebug.query(By.css('.lds-ellipsis')).nativeElement;
+    expect(div).toBeDefined()
+  });
+  it('should be not display error', ()=>{
+    component.isLoading = false;
+    fixture.detectChanges();
+    const errorDebug: DebugElement = fixture.debugElement;
+    const div  = errorDebug.query(By.css('.lds-ellipsis'));
+    console.log(div)
+    expect(div).toBeNull()
   });
 });
--- src/app/shared/loading/loading.service.spec.ts
+++ src/app/shared/loading/loading.service.spec.ts
@@ -1,16 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { LoadingService } from './loading.service';
+import {LoadingService} from './loading.service';
 
 describe('LoadingService', () => {
   let service: LoadingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        LoadingService
+      ]
+    });
     service = TestBed.inject(LoadingService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be True', (done: DoneFn) => {
+    service.load.subscribe(state => {
+      expect(state).toBeTruthy();
+      done();
+    })
+    service.turnOnLoading();
+  });
+
+  it('should be False', (done: DoneFn) => {
+    service.load.subscribe(state => {
+      expect(state).toBeFalsy();
+      done();
+    })
+    service.turnOffLoading()
   });
 });
